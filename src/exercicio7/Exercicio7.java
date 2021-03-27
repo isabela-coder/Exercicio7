a1ac1a9330f01138f66f30acf2edb0aa06d4b727
isabela-coder <81197670+isabela-coder@users.noreply.github.com>
2021-03-27 00:16:10 -0300

    Aula 22-03 - Pacotes MVC criados e Formulario CADPESSOA

---
+++ src/exercicio7/Exercicio7.java
@@ -0,0 +1,27 @@
+
+package exercicio7;
+
+public class Exercicio7 {
+
+    
+    public static void main(String[] args) {
+        
+      double base, altura, areaQuadrado = 28;
+      
+        base =10 ;
+       altura =5;
+       double areaQuadardo = base * altura;
+ 
+ System.out.println("A area de um quadrado de base = " + base);
+ System.out.println("e altura " + altura);
+ System.out.println("é " + areaQuadrado);
+ 
+ }
+}
+    
+
+      
+        
+               
+    
+    
